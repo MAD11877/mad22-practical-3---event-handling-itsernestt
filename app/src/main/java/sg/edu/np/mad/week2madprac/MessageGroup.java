197555143744b7ffe9720e5754f7b127eba59ccf
ernest <s10204577@connect.np.edu.sg>
2022-05-09 21:33:38 +0800

    Done basic tasks, challenge not completed

---
+++ app/src/main/java/sg/edu/np/mad/week2madprac/MessageGroup.java
@@ -0,0 +1,21 @@
+package sg.edu.np.mad.week2madprac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MessageGroup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message_group);
+        //Button button1 = findViewById(R.id.group1);
+        //Button button2 = findViewById(R.id.group2);
+
+
+    }
+}
